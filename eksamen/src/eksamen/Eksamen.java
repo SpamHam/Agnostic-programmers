17cb5505274260955678f3a658b7519c85a5b8ee
peter bærbar <peter brbar@peterbrbar-Pc>
2014-04-28 11:47:58 +0200

    This is jacobs dick22

--- eksamen/src/eksamen/Eksamen.java
+++ eksamen/src/eksamen/Eksamen.java
@@ -13,7 +13,7 @@ package eksamen;
 public class Eksamen {
 
     /**
-     * @param args the command line arguments tttttjaskldaklsdjaklsdjaskd commit that shit
+     * @param args the command line arguments can you hear me now. commit that shit
      */
     public static void main(String[] args) {
         // TODO code application logic here
