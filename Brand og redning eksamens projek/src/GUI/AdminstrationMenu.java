601442ea015ebe4e503db30a104c87ffcc66b4b3
Son Of Satan <Son Of Satan@SonOfSatan-Pc>
2014-04-29 13:22:55 +0200

    Jacobs mor er billigere end det kød mc.D bruger i deres hamburger

--- Brand og redning eksamens projek/src/GUI/AdminstrationMenu.java
+++ Brand og redning eksamens projek/src/GUI/AdminstrationMenu.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package GUI;
 
 /**
@@ -152,7 +151,10 @@ public class AdminstrationMenu extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        CRUDFireman CRUDFiremanFrame = new CRUDFireman();
+        CRUDFiremanFrame.setLocationRelativeTo(this);
+        CRUDFiremanFrame.setVisible(true);
+        dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
