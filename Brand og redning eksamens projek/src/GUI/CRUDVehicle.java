87c51ae5f243efcd9c76e3501cb3bb01189e2ac4
Kathrine <Kathrine@Kathrine-PC>
2014-04-29 15:05:36 +0200

    Opdatering af addMatterialDialog

--- Brand og redning eksamens projek/src/GUI/CRUDVehicle.java
+++ Brand og redning eksamens projek/src/GUI/CRUDVehicle.java
@@ -181,11 +181,12 @@ public class CRUDVehicle extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        AddVehicleDialog tilføjBrandbil = new AddVehicleDialog(this, rootPaneCheckingEnabled);
+        tilføjBrandbil.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
