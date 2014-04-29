3e71712d0993298563b9491019d8a540b91e2639
Son Of Satan <Son Of Satan@SonOfSatan-Pc>
2014-04-29 15:01:02 +0200

    Adminstration Menu Finished.

--- Brand og redning eksamens projek/src/GUI/AdminstrationMenu.java
+++ Brand og redning eksamens projek/src/GUI/AdminstrationMenu.java
@@ -164,7 +164,10 @@ public class AdminstrationMenu extends javax.swing.JFrame {
         dispose();    }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        CRUDMaterial CRUDMaterialFrame = new CRUDMaterial();
+        CRUDMaterialFrame.setLocationRelativeTo(this);
+        CRUDMaterialFrame.setVisible(true);
+        dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
