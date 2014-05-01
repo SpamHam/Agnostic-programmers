5dcc74f497d33c2d26bad2d750567bb2ad696ade
Son Of Satan <Son Of Satan@SonOfSatan-Pc>
2014-05-01 13:07:31 +0200

    #ThisHashTagThingIsGettingSoMuchOutOfHand

---
+++ Brand og redning eksamens projek/src/BE/BEFireman.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package BE;
+
+/**
+ *
+ * @author Son Of Satan
+ */
+public class BEFireman {
+    private String CPR, FirstName, LastName, Address;
+    private int PhoneNr, CallNr, PaymentNr;
+    private boolean LeaderTrained;
+    
+    public BEFireman(String CPR, String FirstName, String LastName, String Address, int  PhoneNr, int CallNr, int PaymentNr, boolean LeaderTrained){
+        this.setCPR(CPR);
+        this.setFirstName(FirstName);
+        this.setLastName(LastName);
+        this.setAddress(Address);
+        this.setPhoneNr(PhoneNr);
+        this.setCallNr(CallNr);
+        this.setPaymentNr(PaymentNr);
+        this.setLeaderTrained(LeaderTrained);
+    }
+
+    /**
+     * @return the CPR
+     */
+    public String getCPR() {
+        return CPR;
+    }
+
+    /**
+     * @param CPR the CPR to set
+     */
+    public void setCPR(String CPR) {
+        this.CPR = CPR;
+    }
+
+    /**
+     * @return the FirstName
+     */
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    /**
+     * @param FirstName the FirstName to set
+     */
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    /**
+     * @return the LastName
+     */
+    public String getLastName() {
+        return LastName;
+    }
+
+    /**
+     * @param LastName the LastName to set
+     */
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    /**
+     * @return the Address
+     */
+    public String getAddress() {
+        return Address;
+    }
+
+    /**
+     * @param Address the Address to set
+     */
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
+    /**
+     * @return the PhoneNr
+     */
+    public int getPhoneNr() {
+        return PhoneNr;
+    }
+
+    /**
+     * @param PhoneNr the PhoneNr to set
+     */
+    public void setPhoneNr(int PhoneNr) {
+        this.PhoneNr = PhoneNr;
+    }
+
+    /**
+     * @return the CallNr
+     */
+    public int getCallNr() {
+        return CallNr;
+    }
+
+    /**
+     * @param CallNr the CallNr to set
+     */
+    public void setCallNr(int CallNr) {
+        this.CallNr = CallNr;
+    }
+
+    /**
+     * @return the PaymentNr
+     */
+    public int getPaymentNr() {
+        return PaymentNr;
+    }
+
+    /**
+     * @param PaymentNr the PaymentNr to set
+     */
+    public void setPaymentNr(int PaymentNr) {
+        this.PaymentNr = PaymentNr;
+    }
+
+    /**
+     * @return the LeaderTrained
+     */
+    public boolean isLeaderTrained() {
+        return LeaderTrained;
+    }
+
+    /**
+     * @param LeaderTrained the LeaderTrained to set
+     */
+    public void setLeaderTrained(boolean LeaderTrained) {
+        this.LeaderTrained = LeaderTrained;
+    }
+    
+}
---
+++ Brand og redning eksamens projek/src/DALC/DALCFireman.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DALC;
+
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Morten H.
+ */
+public class DALCFireman {
+
+    private static DALCFireman m_instance;
+    Connection m_connection;
+
+    /**
+     * Singleton
+     *
+     * @return
+     */
+    public static DALCFireman getInstance() throws SQLServerException {
+        if (m_instance == null) {
+            m_instance = new DALCFireman();
+        }
+        return m_instance;
+    }
+
+    /**
+     * calls for a new instance of the connection
+     *
+     */
+    private DALCFireman() throws SQLServerException {
+        m_connection = DALC.DBConnection.getInstance().getConnection();
+    }
+
+    /**
+     * Creates an row in DALCCar table.
+     *
+     * @param e
+     * @throws SQLException
+     */
+    public void Create(BE.BEFireman e) throws SQLException {
+        String sql = "insert into Fireman values (?,?,?,?,?,?,?,?)";
+        PreparedStatement ps = m_connection.prepareStatement(sql);
+        ps.setString(1, e.getCPR());
+        ps.setString(2, e.getFirstName());
+        ps.setString(3, e.getLastName());
+        ps.setString(4, e.getAddress());
+        ps.setInt(5, e.getPhoneNr());
+        ps.setInt(6, e.getCallNr());
+        ps.setInt(7, e.getPaymentNr());
+        ps.setBoolean(8, e.isLeaderTrained());
+        ps.executeUpdate();
+    }
+
+    /**
+     * Reads all rows from car table.
+     *
+     * @return
+     * @throws SQLException
+     */
+    public ArrayList<BE.BEFireman> read() throws SQLException {
+        ArrayList<BE.BEFireman> res = new ArrayList<>();
+        Statement stm = m_connection.createStatement();
+        if (!stm.execute("select * from Fireman")) {
+            throw new SQLException("Could not load from Fireman table");
+        }
+        ResultSet result = stm.getResultSet();
+        while (result.next()) {
+
+            String CPR = result.getString("CPR");
+            String FirstName = result.getString("FirstName");
+            String LastName = result.getString("LastName");
+            String Address = result.getString("Address");
+            int PhoneNr = result.getInt("PhoneNr");
+            int CallNr = result.getInt("CallNr");
+            int PaymentNr = result.getInt("PaymentNr");
+            boolean isLeaderTrained = result.getBoolean("isLeaderTrained");
+            
+            BE.BEFireman c = new BE.BEFireman(CPR, FirstName, LastName, Address, PhoneNr, CallNr, PaymentNr, isLeaderTrained);
+            res.add(c);
+        }
+        return res;
+    }
+
+    /**
+     * 
+     * @param u
+     * @throws SQLException 
+     */
+    public void update(BE.BEFireman u) throws SQLException {
+        String sql = "update Fireman set FirstName=?, LastName=?, Address=?, PhoneNr=?, CallNr=?, PaymentNr=?, LeaderTrained=? where CPR=?";
+        PreparedStatement ps = m_connection.prepareStatement(sql);
+        ps.setString(1, u.getFirstName());
+        ps.setString(2, u.getLastName());
+        ps.setString(3, u.getAddress());
+        ps.setInt(4, u.getPhoneNr());
+        ps.setInt(5, u.getCallNr());
+        ps.setInt(6, u.getPaymentNr());
+        ps.setBoolean(7, u.isLeaderTrained());
+        ps.setString(8, u.getCPR());
+        ps.executeUpdate();
+    }
+    
+    /**
+     * Removes an specifik row from car table.
+     *
+     * @param e
+     * @throws java.sql.SQLException
+     */
+        public void Delete(BE.BEFireman e) throws SQLException {
+    String sql = "delete from Fireman where CPR=?";
+    PreparedStatement ps = m_connection.prepareStatement(sql);
+    ps.setString(1, e.getCPR());
+    ps.executeUpdate();
+    }
+}
