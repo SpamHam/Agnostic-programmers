b836e8b64708e197ea1ac2f22d29faab33caf1ba
Son Of Satan <Son Of Satan@SonOfSatan-Pc>
2014-05-01 15:01:25 +0200

    Java for beginners :P

---
+++ Brand og redning eksamens projek/src/BE/BESalary.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BE;
+
+/**
+ *
+ * @author Son Of Satan
+ */
+public class BESalary {
+
+    private String CPR, Role, Date, TypeOfWork;
+    private int SalaryCode, Hours, ODIN;
+    private boolean isHoliday;
+    
+    public BESalary(int ODIN, String CPR, String Role, int SalaryCode, int Hours, String Date, String TypeOfWork, boolean isHoliday){
+        this.ODIN = ODIN;
+        this.CPR = CPR;
+        this.Role = Role;
+        this.SalaryCode = SalaryCode;
+        this.Hours = Hours;
+        this.Date = Date;
+        this.TypeOfWork = TypeOfWork;
+        this.isHoliday = isHoliday;
+    }
+
+    /**
+     * @return the CPR
+     */
+    public String getCPR() {
+        return CPR;
+    }
+
+    /**
+     * @param CPR the CPR to set
+     */
+    public void setCPR(String CPR) {
+        this.CPR = CPR;
+    }
+
+    /**
+     * @return the Role
+     */
+    public String getRole() {
+        return Role;
+    }
+
+    /**
+     * @param Role the Role to set
+     */
+    public void setRole(String Role) {
+        this.Role = Role;
+    }
+
+    /**
+     * @return the Date
+     */
+    public String getDate() {
+        return Date;
+    }
+
+    /**
+     * @param Date the Date to set
+     */
+    public void setDate(String Date) {
+        this.Date = Date;
+    }
+
+    /**
+     * @return the TypeOfWork
+     */
+    public String getTypeOfWork() {
+        return TypeOfWork;
+    }
+
+    /**
+     * @param TypeOfWork the TypeOfWork to set
+     */
+    public void setTypeOfWork(String TypeOfWork) {
+        this.TypeOfWork = TypeOfWork;
+    }
+
+    /**
+     * @return the SalaryCode
+     */
+    public int getSalaryCode() {
+        return SalaryCode;
+    }
+
+    /**
+     * @param SalaryCode the SalaryCode to set
+     */
+    public void setSalaryCode(int SalaryCode) {
+        this.SalaryCode = SalaryCode;
+    }
+
+    /**
+     * @return the Hours
+     */
+    public int getHours() {
+        return Hours;
+    }
+
+    /**
+     * @param Hours the Hours to set
+     */
+    public void setHours(int Hours) {
+        this.Hours = Hours;
+    }
+
+    /**
+     * @return the isHoliday
+     */
+    public boolean isIsHoliday() {
+        return isHoliday;
+    }
+
+    /**
+     * @param isHoliday the isHoliday to set
+     */
+    public void setIsHoliday(boolean isHoliday) {
+        this.isHoliday = isHoliday;
+    }
+
+    /**
+     * @return the ODIN
+     */
+    public int getODIN() {
+        return ODIN;
+    }
+
+    /**
+     * @param ODIN the ODIN to set
+     */
+    public void setODIN(int ODIN) {
+        this.ODIN = ODIN;
+    }
+}
---
+++ Brand og redning eksamens projek/src/DALC/DALCSalary.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DALC;
+
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Morten H.
+ */
+public class DALCSalary {
+
+    private static DALCSalary m_instance;
+    Connection m_connection;
+
+    /**
+     * Singleton
+     *
+     * @return
+     * @throws com.microsoft.sqlserver.jdbc.SQLServerException
+     */
+    public static DALCSalary getInstance() throws SQLServerException {
+        if (m_instance == null) {
+            m_instance = new DALCSalary();
+        }
+        return m_instance;
+    }
+
+    /**
+     * calls for a new instance of the connection
+     *
+     */
+    private DALCSalary() throws SQLServerException {
+        m_connection = DALC.DBConnection.getInstance().getConnection();
+    }
+
+    /**
+     * Creates an row in DALCCar table.
+     *
+     * @param e
+     * @throws SQLException
+     */
+    public void CreateMonthly(BE.BESalary e) throws SQLException {
+        String sql = "insert into MonthlySalary values (?,?,?,?,?)";
+        PreparedStatement ps = m_connection.prepareStatement(sql);
+        ps.setInt(1, e.getODIN());
+        ps.setString(2, e.getCPR());
+        ps.setString(3, e.getRole());
+        ps.setInt(4, e.getSalaryCode());
+        ps.setInt(5, e.getHours());
+        ps.executeUpdate();
+    }
+
+    public void CreateSalary(BE.BESalary e) throws SQLException {
+        String sql = "insert into SalaryReport values (?,?,?,?)";
+        PreparedStatement ps = m_connection.prepareStatement(sql);
+        ps.setInt(1, e.getODIN());
+        ps.setString(2, e.getDate());
+        ps.setString(3, e.getTypeOfWork());
+        ps.setBoolean(4, e.isIsHoliday());
+        ps.executeUpdate();
+
+    }
+
+    /**
+     * Reads all rows from car table.
+     *
+     * @return
+     * @throws SQLException
+     */
+    public ArrayList<BE.BESalary> read() throws SQLException {
+        ArrayList<BE.BESalary> res = new ArrayList<>();
+        Statement stm = m_connection.createStatement();
+        if (!stm.execute("select * from MonthlySalary inner join SalaryReport on SalaryReport.OdinNr = MonthlySalary.ODINnr")) {
+            throw new SQLException("Could not load from MontlySalary or SalaryReport table");
+        }
+        ResultSet result = stm.getResultSet();
+        while (result.next()) {
+
+            int ODIN = result.getInt("ODIN");
+            String CPR = result.getString("CPR");
+            String Role = result.getString("Role");
+            int SalaryCode = result.getInt("SalaryCode");
+            int Hours = result.getInt("Hours");
+            String Date = result.getString("Date");
+            String TypeOfWork = result.getString("TypeOfWork");
+            boolean isHoliday = result.getBoolean("isHoliday");
+
+            BE.BESalary c = new BE.BESalary(ODIN, CPR, Role, SalaryCode, Hours, Date, TypeOfWork, isHoliday);
+            res.add(c);
+        }
+        return res;
+    }
+
+    /**
+     * Removes an specifik row from car table.
+     *
+     * @param e
+     * @throws java.sql.SQLException
+     */
+    public void DeleteMonthly(BE.BESalary e) throws SQLException {
+        String sql = "delete from MonthlySalary, SalaryReport where ODINnr=?";
+        PreparedStatement ps = m_connection.prepareStatement(sql);
+        ps.setInt(1, e.getODIN());
+        ps.executeUpdate();
+    }
+}
