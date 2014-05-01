2599d5b6062fcd006b4659d597b3ca053dbf2d4d
peter bærbar <peter brbar@peterbrbar-Pc>
2014-05-01 14:35:47 +0200

    Merge WorkingSoftware/Sprint1
    Conflicts:
    	Brand og redning eksamens projek/src/GUI/Timeplan.java

--- Brand og redning eksamens projek/src/GUI/AdminstrationMenu.java
+++ Brand og redning eksamens projek/src/GUI/AdminstrationMenu.java
@@ -172,6 +172,7 @@ public class AdminstrationMenu extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+
         CRUDVehicle CRUDVehicleFrame = new CRUDVehicle();
         CRUDVehicleFrame.setLocationRelativeTo(this);
         CRUDVehicleFrame.setVisible(true);
