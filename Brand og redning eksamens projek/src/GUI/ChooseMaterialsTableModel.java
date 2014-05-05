c0d770197c2c69e215f7a826e2f2eb3598b32ff9
Claus <Claus@192.168.199.1>
2014-05-05 11:55:02 +0200

    Sprint 1:
    Opdatering af ODIN Report

--- Brand og redning eksamens projek/src/GUI/ChooseMaterialsTableModel.java
+++ Brand og redning eksamens projek/src/GUI/ChooseMaterialsTableModel.java
@@ -6,6 +6,8 @@
 
 package GUI;
 
+import BE.BEMaterial;
+import java.util.ArrayList;
 import javax.swing.table.AbstractTableModel;
 
 /**
@@ -17,6 +19,34 @@ public class ChooseMaterialsTableModel extends AbstractTableModel {
 private String[] colNames = {"Materiale", "Antal"};
 private Class[] classes = {String.class, Integer.class};
 
+private ArrayList<BEMaterial> allSelectedMaterials;
+
+    public ChooseMaterialsTableModel(ArrayList<BEMaterial> selectedMaterials) {
+        allSelectedMaterials = selectedMaterials;
+        fireTableDataChanged();
+    }
+
+        @Override
+    public int getRowCount() {
+        return allSelectedMaterials.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colNames.length;
+    }
+
+    @Override
+    public String getColumnName(int col) {
+
+        return colNames[col];
+    }
+
+    @Override
+    public Class<?> getColumnClass(int col) {
+        return classes[col];
+    }
+    
     @Override
     public boolean isCellEditable(int row, int col) {
          switch (col) {
@@ -26,5 +56,37 @@ private Class[] classes = {String.class, Integer.class};
              return false;
          }
     }
+
+    public void setMaterialsStatusList(ArrayList<BEMaterial> materialList) {
+        allSelectedMaterials = materialList;
+    }    
     
+     public BEMaterial getTimePlanByRow(int row) {
+        return allSelectedMaterials.get(row);
+    }
+   
+     
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        BEMaterial e = allSelectedMaterials.get(rowIndex);
+            switch (columnIndex) {
+                case 0:
+                    return e.getM_Materiale();
+                case 1:
+                    return e.getM_Antal();
+            }
+
+            return null;
+        }     
+
+      @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex)
+    {
+        BEMaterial row = allSelectedMaterials.get(rowIndex);
+        switch (columnIndex){
+            case 1: 
+              row.setM_Antal((Integer) aValue);
+    } 
+   }
+
 }
