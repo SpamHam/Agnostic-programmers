0b1c621888a7d5a3330cb56f4c1352fcbdc6aa7f
Son Of Satan <Son Of Satan@SonOfSatan-Pc>
2014-05-05 15:01:53 +0200

    Hail Arla, our enternal god.

--- Brand og redning eksamens projek/src/BLL/BLLPayroll.java
+++ Brand og redning eksamens projek/src/BLL/BLLPayroll.java
@@ -5,10 +5,76 @@
  */
 package BLL;
 
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.util.ArrayList;
+
 /**
  *
- * @author peter bærbar
+ * @author Morten H.
  */
 public class BLLPayroll {
 
+    private static BLLPayroll m_instance;
+    private DALC.DALCSalary DALCSalary;
+    private final Utility.ErrorHandler Error;
+
+    /**
+     * Singleton
+     *
+     * @return
+     * @throws SQLServerException
+     */
+    public static BLLPayroll getInstance() throws Exception {
+        if (m_instance == null) {
+            m_instance = new BLLPayroll();
+        }
+        return m_instance;
+    }
+
+    private BLLPayroll() throws Exception {
+        Error = Utility.ErrorHandler.getInstance();
+        try {
+            DALCSalary = DALC.DALCSalary.getInstance();
+        } catch (SQLServerException ex) {
+            Error.StorageUnreachable(".");
+        }
+    }
+
+    public void CreateSalary(BE.BESalary b) throws Exception {
+        if (b.getODIN() == 0 || b.getTypeOfWork().isEmpty() || b.getDate().isEmpty()) {
+            Error.NotEnougthInfo("creating a SalaryReport.");
+        } else {
+            try {
+                DALCSalary.getInstance().CreateSalary(b);
+            } catch (SQLServerException ex) {
+                Error.StorageUnreachable(".");
+            }
+        }
+    }
+
+    public void CreateMonthly(BE.BESalary b) throws Exception {
+        if (b.getODIN() == 0 || b.getCPR().isEmpty() || b.getRole().isEmpty() || b.getSalaryCode() == 0 || b.getHours() == 0) {
+            Error.NotEnougthInfo("creating a SalaryReport.");
+        } else {
+            try {
+                DALCSalary.getInstance().CreateSalary(b);
+            } catch (SQLServerException ex) {
+                Error.StorageUnreachable(".");
+            }
+        }
+    }
+
+    public ArrayList<BE.BESalary> getAll() throws Exception {
+        ArrayList<BE.BESalary> res = new ArrayList<>();
+        try {
+            res = DALCSalary.getInstance().read();
+        } catch (SQLServerException ex) {
+            Error.StorageUnreachable(".");
+        }
+        return res;
+    }
+
+    public void remove() throws Exception {
+        //TODO after Salary are done.
+    }
 }
