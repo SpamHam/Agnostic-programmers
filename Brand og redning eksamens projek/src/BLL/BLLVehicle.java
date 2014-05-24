8c059bf691160c51a74df45c4e3f9f98793e1c9e
peter bærbar <peter brbar@peterbrbar-Pc>
2014-05-24 21:57:38 +0200

    Observer pattern for crud material

--- Brand og redning eksamens projek/src/BLL/BLLVehicle.java
+++ Brand og redning eksamens projek/src/BLL/BLLVehicle.java
@@ -9,11 +9,8 @@ import BE.BEVehicle;
 import DALC.IDALCVehicle;
 import GUI.VehicleListener;
 import Utility.Error.EventExercutionException;
-import com.microsoft.sqlserver.jdbc.SQLServerException;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 /**
  *
@@ -22,57 +19,12 @@ import java.util.logging.Logger;
 public class BLLVehicle implements VehicleListener{
 
     IDALCVehicle m_dal; 
-    private DALC.DALCVehicle DALCVehicle;
-    private final Utility.Error.ErrorHandler Error;
 
-    /**
-     * Singleton to ensure that the class isn't instantiated more than once
-     *
-     * @return m_instance
-     * @throws SQLServerException
-     */
-//    public static BLLVehicle getInstance() throws Exception {
-//        if (m_instance == null) {
-//            m_instance = new BLLVehicle();
-//        }
-//        return m_instance;
-//    }
-
-    /**
-     * Instantiates the DALC Layer using the Singleton Pattern
-     * @throws Exception 
-     */
-//    private BLLVehicle() throws Exception {
-//        Error = Utility.Error.ErrorHandler.getInstance();
-//        try {
-//            DALCVehicle = DALC.DALCVehicle.getInstance();
-//        } catch (SQLServerException ex) {
-//            Error.StorageUnreachable(".");
-//        }
-//    }
     
     public BLLVehicle(){
-    Error = Utility.Error.ErrorHandler.getInstance();
     }
     
      public void setDAlC(IDALCVehicle m_dal){this.m_dal = m_dal;}
-
-    /**
-     * Function that calls the create function from the DALC Layer. If any field is empty the function will return an error
-     * @param b
-     * @throws Exception 
-     */
-//    public void Create(BE.BEVehicle b) throws Exception {
-//        if (b.getM_registrationNr().isEmpty() || b.getM_mærke().isEmpty() || b.getM_model().isEmpty()) {
-//            Error.NotEnougthInfo("creating a vehicle");
-//        } else {
-//            try {
-//                DALCVehicle.Create(b);
-//            } catch (SQLServerException e) {
-//            Error.StorageUnreachable(".");
-//            }
-//        }
-//    }
 
     /**
      * A function that retrieves all fireman info from the database and inserts it into an ArrayList
@@ -82,40 +34,13 @@ public class BLLVehicle implements VehicleListener{
     public ArrayList<BE.BEVehicle> getAll() throws Exception {
         ArrayList<BE.BEVehicle> res = new ArrayList<>();
         try {
-           // res = DALCVehicle.read();
           res =  m_dal.VehicleReadPerformed();
         } catch (EventExercutionException ex) {
-           // Error.StorageUnreachable(".");
             throw new EventExercutionException(ex.getMessage());
         }
         return res;
     }
 
-    /**
-     * A function that calls the update function from the DALC Layer using Singleton
-     * If any field is left blank it will return an error
-     * @param b
-     * @throws Exception 
-     */
-//    public void Update(BE.BEVehicle b) throws Exception {
-//        if (b.getM_registrationNr().isEmpty() || b.getM_mærke().isEmpty() || b.getM_model().isEmpty()) {
-//            Error.NotEnougthInfo("updating a vehicle.");
-//        } else {
-//            try {
-//                DALCVehicle.getInstance().update(b);
-//            } catch (SQLServerException ex) {
-//            Error.StorageUnreachable(".");
-//            }
-//        }
-//    }
-
-    /**
-     * A function that calls the delete function from the DALC Layer using Singleton
-     * @throws Exception 
-     */
-//    public void remove(BE.BEVehicle e) throws Exception {
-//        DALCVehicle.Delete(e);
-//    }
 
     @Override
     public void VehicleCreatePerformed(BEVehicle event) {
@@ -123,8 +48,6 @@ public class BLLVehicle implements VehicleListener{
             throw new EventExercutionException("Ikke nok info til at oprette Køretøj");
         } else {
             try {
-               // DALCVehicle.Create(event);
-               // DALCVehicle.VehicleCreatePerformed(event);
                 m_dal.VehicleCreatePerformed(event);
             } catch (EventExercutionException e) {
             throw new EventExercutionException(e.getMessage());
@@ -148,7 +71,6 @@ public class BLLVehicle implements VehicleListener{
             throw new EventExercutionException("Ikke nok info til at oprette Køretøj");
         } else {
             try {
-                //DALCVehicle.update(event);
                 m_dal.VehicleUpdatePerformed(event);
             } catch (EventExercutionException ex) {
             throw new EventExercutionException(ex.getMessage());
