f93410c31e0fc977c85f78c0a135f01b7db823b9
peter bærbar <peter brbar@peterbrbar-Pc>
2014-06-02 11:56:01 +0200

    Observer pattern for crud material

--- Brand og redning eksamens projek/src/GUI/CRUDVehicle.java
+++ Brand og redning eksamens projek/src/GUI/CRUDVehicle.java
@@ -17,8 +17,6 @@ import com.microsoft.sqlserver.jdbc.SQLServerException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
